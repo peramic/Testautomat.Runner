8fc851548e9bc1510fd88931d459133446bb1632
Viktor Abrams <abrams@peramic.io>
2021-03-10 16:57:15 +0100

    Release

---
+++ src/main/java/havis/test/suite/TestautomatRunner.java
@@ -0,0 +1,15 @@
+package havis.test.suite;
+
+import havis.test.suite.Testautomat;
+
+
+public class TestautomatRunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Testautomat().run(args);
+	}
+
+}
---
+++ src/main/resources/zip/testautomat.bat
@@ -0,0 +1,1 @@
+java -jar havis.testautomat.runner.jar UICommon.CLI
